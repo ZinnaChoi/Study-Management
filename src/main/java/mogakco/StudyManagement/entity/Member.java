b2ac02c2ea25db02ae9ca3ba4feb71ed067c9d62
Zinna <choiyh11097@gmail.com>
2024-01-04 00:34:10 +0900

    [feat] 로그인 기능 생성을 위한 Member JPA entity 추가

---
+++ src/main/java/mogakco/StudyManagement/entity/Member.java
@@ -0,0 +1,49 @@
+package mogakco.StudyManagement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mogakco.StudyManagement.enums.MemberRole;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "member")
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long memberId;
+
+  @Column(nullable = false, unique = true)
+  private String id;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String contact;
+
+  @Column(nullable = false)
+  private MemberRole role;
+
+  @Column(nullable = false)
+  private String createdAt;
+
+  @Column(nullable = false)
+  private String updatedAt;
+
+  private String expiredAt;
+}
