dcab0bc84e706c0da0f438bd7f066c214bc67b88
MeMyself-And-I <leech_98@naver.com>
2024-01-10 21:49:00 +0900

    [fix] properties change and login api return value change

--- src/main/java/mogakco/StudyManagement/service/member/MemberService.java
+++ src/main/java/mogakco/StudyManagement/service/member/MemberService.java
@@ -4,9 +4,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import mogakco.StudyManagement.dto.MemberLoginReq;
+import mogakco.StudyManagement.dto.MemberLoginRes;
 
 public interface MemberService {
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 
-    String login(MemberLoginReq loginInfo);
+    MemberLoginRes login(MemberLoginReq loginInfo);
 }
