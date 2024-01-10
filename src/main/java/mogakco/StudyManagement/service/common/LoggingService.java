519d368d72a3f006348aa77859208f275a14558d
Zinna <choiyh11097@gmail.com>
2024-01-10 23:00:22 +0900

    [feat] Define Logging Service

---
+++ src/main/java/mogakco/StudyManagement/service/common/LoggingService.java
@@ -0,0 +1,15 @@
+package mogakco.StudyManagement.service.common;
+
+public interface LoggingService {
+
+    void setAPIStart();
+
+    void setAPIEnd();
+
+    void setDBStart();
+
+    void setDBEnd();
+
+    void logDuration();
+
+}
---
+++ src/main/java/mogakco/StudyManagement/service/common/impl/LoggingServiceImpl.java
@@ -0,0 +1,55 @@
+package mogakco.StudyManagement.service.common.impl;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import lombok.Getter;
+import lombok.Setter;
+import mogakco.StudyManagement.service.common.LoggingService;
+
+@Getter
+@Setter
+@Service
+public class LoggingServiceImpl implements LoggingService {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingServiceImpl.class);
+
+    private Long timeAPI = 0L;
+    private Long timeDB = 0L;
+    private Long dbStartTime = 0L;
+
+    @Override
+    public void setAPIStart() {
+        timeAPI = System.currentTimeMillis();
+    }
+
+    @Override
+    public void setAPIEnd() {
+        timeAPI = System.currentTimeMillis() - timeAPI;
+        logDuration();
+        resetDurations();
+    }
+
+    @Override
+    public void setDBStart() {
+        dbStartTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void setDBEnd() {
+        long duration = System.currentTimeMillis() - dbStartTime;
+        timeDB += duration;
+    }
+
+    @Override
+    public void logDuration() {
+        logger.info("API Duration: {} ms, DB Duration: {} ms", timeAPI, timeDB);
+    }
+
+    private void resetDurations() {
+        timeAPI = 0L;
+        timeDB = 0L;
+        dbStartTime = 0L;
+    }
+}
