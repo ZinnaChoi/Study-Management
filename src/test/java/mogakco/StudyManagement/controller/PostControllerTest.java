184c4f66447404c7a22f0ea566e5688be54bca21
Zinna <choiyh11097@gmail.com>
2024-01-15 22:02:13 +0900

    [chore] Add DisplayName to post test and get systemId from properties

--- src/test/java/mogakco/StudyManagement/controller/PostControllerTest.java
+++ src/test/java/mogakco/StudyManagement/controller/PostControllerTest.java
@@ -1,12 +1,13 @@
 package mogakco.StudyManagement.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -32,6 +33,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 @SpringBootTest
 @Transactional
+@DisplayName("게시글 테스트")
 public class PostControllerTest {
 
     @Mock
@@ -52,14 +54,18 @@ public class PostControllerTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Value("${study.systemId}")
+    private String systemId;
+
     private static final String POST_API_URL = "/api/v1/posts";
 
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 등록 성공")
     public void createPostSuccess() throws Exception {
         String requestBodyJson = objectMapper
-                .writeValueAsString(new PostReq(DateUtil.getCurrentDateTime(), "SYS_01", "2024 2월 개발 뉴스 공유드립니다",
+                .writeValueAsString(new PostReq(DateUtil.getCurrentDateTime(), systemId, "2024 2월 개발 뉴스 공유드립니다",
                         "chatGPT 5.0 도입"));
         TestUtil.performRequest(mockMvc, POST_API_URL, requestBodyJson, "POST", 200, 200);
     }
@@ -67,19 +73,21 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 등록 실패 - 빈 제목")
     public void createPostFailEmptyTitle() throws Exception {
         String requestBodyJson = objectMapper
-                .writeValueAsString(new PostReq(DateUtil.getCurrentDateTime(), "SYS_01", null, "chatGPT 5.0 도입"));
+                .writeValueAsString(new PostReq(DateUtil.getCurrentDateTime(), systemId, null, "chatGPT 5.0 도입"));
         TestUtil.performRequest(mockMvc, POST_API_URL, requestBodyJson, "POST", 400, 400);
     }
 
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 등록 실패 - 잘못된 요청 JSON 형식")
     public void createPostFailInvalidJson() throws Exception {
         String invalidJson = "{\n" +
                 "  \"sendDate\": \"20240112132910401\",\n" +
-                "  \"systemId\": \"SYS_01\",\n" +
+                "  \"systemId\": \"ABC\",\n" +
                 "  \"title\": \"2024 2월 개발 뉴스 공유드립니다\",\n" +
                 "  \"content\": \"chatGPT 5.0 도입\"\n"; // Bad Request (Missing closing brace)
 
@@ -91,12 +99,13 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 목록 조회 성공 - Page 0 Size 3")
     public void getPostListSuccessPage0Size3() throws Exception {
 
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(POST_API_URL);
 
         uriBuilder.queryParam("sendDate", DateUtil.getCurrentDateTime())
-                .queryParam("systemId", "SYS_01")
+                .queryParam("systemId", systemId)
                 .queryParam("searchKeyWord", "post")
                 .queryParam("searchType", PostSearchType.TITLE)
                 .queryParam("page", 0)
@@ -104,16 +113,12 @@ public class PostControllerTest {
                 .queryParam("sort", "title,desc");
 
         MvcResult result = TestUtil.performRequest(mockMvc, uriBuilder.toUriString(), null, "GET", 200, 200);
+        JsonNode responseBody = objectMapper.readTree(result.getResponse().getContentAsString());
 
-        String content = result.getResponse().getContentAsString();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(content);
-
-        int contentCount = rootNode.path("content").size();
+        int contentCount = responseBody.path("content").size();
         assertTrue(contentCount == 3);
 
-        for (JsonNode element : rootNode.path("content")) {
+        for (JsonNode element : responseBody.path("content")) {
             String title = element.path("title").asText();
             assertTrue(title.startsWith("post4"));
             break;
@@ -123,12 +128,13 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 목록 조회 성공 - 작성자 이름으로 조회")
     public void getPostListSuccessSearchMember() throws Exception {
 
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(POST_API_URL);
 
         uriBuilder.queryParam("sendDate", DateUtil.getCurrentDateTime())
-                .queryParam("systemId", "SYS_01")
+                .queryParam("systemId", systemId)
                 .queryParam("searchKeyWord", "PostUser")
                 .queryParam("searchType", PostSearchType.MEMBER)
                 .queryParam("page", 0)
@@ -136,25 +142,22 @@ public class PostControllerTest {
                 .queryParam("sort", "title,desc");
 
         MvcResult result = TestUtil.performRequest(mockMvc, uriBuilder.toUriString(), null, "GET", 200, 200);
+        JsonNode responseBody = objectMapper.readTree(result.getResponse().getContentAsString());
 
-        String content = result.getResponse().getContentAsString();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(content);
-
-        int contentCount = rootNode.path("content").size();
+        int contentCount = responseBody.path("content").size();
         assertTrue(contentCount == 4);
     }
 
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 목록 조회 성공 - Page 1 Size 2")
     public void getPostListSuccessPage1Size2() throws Exception {
 
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(POST_API_URL);
 
         uriBuilder.queryParam("sendDate", DateUtil.getCurrentDateTime())
-                .queryParam("systemId", "SYS_01")
+                .queryParam("systemId", systemId)
                 .queryParam("searchKeyWord", "post")
                 .queryParam("searchType", PostSearchType.TITLE)
                 .queryParam("page", 1)
@@ -177,12 +180,13 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 목록 조회 실패 - 잘못된 검색 타입")
     public void getPostListFailInvalidSearchType() throws Exception {
 
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(POST_API_URL);
 
         uriBuilder.queryParam("sendDate", DateUtil.getCurrentDateTime())
-                .queryParam("systemId", "SYS_01")
+                .queryParam("systemId", systemId)
                 .queryParam("searchKeyWord", "post")
                 .queryParam("searchType", "INVALID_TYPE") // Invalid SearchType
                 .queryParam("page", 0)
@@ -195,12 +199,13 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 목록 조회 실패 - 빈 검색어")
     public void getPostListFailEmptySearchKeyWord() throws Exception {
 
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(POST_API_URL);
 
         uriBuilder.queryParam("sendDate", DateUtil.getCurrentDateTime())
-                .queryParam("systemId", "SYS_01")
+                .queryParam("systemId", systemId)
                 .queryParam("searchKeyWord", "")
                 .queryParam("searchType", PostSearchType.TITLE)
                 .queryParam("page", 0)
@@ -215,10 +220,11 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 수정 성공")
     public void updatePostSuccess() throws Exception {
 
         String requestBodyJson = objectMapper.writeValueAsString(
-                new PostReq(DateUtil.getCurrentDateTime(), "SYS_01", "Updated Title", "Updated Content"));
+                new PostReq(DateUtil.getCurrentDateTime(), systemId, "Updated Title", "Updated Content"));
 
         MvcResult result = TestUtil.performRequest(mockMvc,
                 POST_API_URL + "/" + getLatestPostIdByMemberId("PostUser"), requestBodyJson, "PATCH", 200, 200);
@@ -234,9 +240,10 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 수정 실패 - 잘못된 게시글 번호")
     public void updatePostFailPostNotFound() throws Exception {
         String requestBodyJson = objectMapper.writeValueAsString(
-                new PostReq(DateUtil.getCurrentDateTime(), "SYS_01", "Updated Title", "Updated Content"));
+                new PostReq(DateUtil.getCurrentDateTime(), systemId, "Updated Title", "Updated Content"));
 
         // Not Exist postId -1
         TestUtil.performRequest(mockMvc,
@@ -245,11 +252,12 @@ public class PostControllerTest {
 
     @Test
     @Sql("/post/PostListSetup.sql")
-    @WithMockUser(username = "PostUser2", authorities = { "USER" }) // Differnet Member
+    @WithMockUser(username = "PostUser2", authorities = { "USER" })
+    @DisplayName("게시글 수정 실패 - 잘못된 사용자")
     public void updatePostFailPostDiffMember() throws Exception {
 
         String requestBodyJson = objectMapper.writeValueAsString(
-                new PostReq(DateUtil.getCurrentDateTime(), "SYS_01", "Updated Title", "Updated Content"));
+                new PostReq(DateUtil.getCurrentDateTime(), systemId, "Updated Title", "Updated Content"));
 
         TestUtil.performRequest(mockMvc,
                 POST_API_URL + "/" + getLatestPostIdByMemberId("PostUser"), requestBodyJson, "PATCH", 200, 400);
@@ -260,6 +268,7 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 삭제 성공")
     public void deletePostSuccess() throws Exception {
 
         TestUtil.performRequest(mockMvc,
@@ -269,6 +278,7 @@ public class PostControllerTest {
     @Test
     @Sql("/post/PostListSetup.sql")
     @WithMockUser(username = "PostUser", authorities = { "USER" })
+    @DisplayName("게시글 삭제 실패 - 잘못된 게시글 번호")
     public void deletePostFailNotFound() throws Exception {
         TestUtil.performRequest(mockMvc,
                 POST_API_URL + "/" + -1, null, "DELETE", 200, 404); // Not Exist PostId
@@ -276,7 +286,8 @@ public class PostControllerTest {
 
     @Test
     @Sql("/post/PostListSetup.sql")
-    @WithMockUser(username = "PostUser2", authorities = { "USER" }) // Differnet Member
+    @WithMockUser(username = "PostUser2", authorities = { "USER" })
+    @DisplayName("게시글 삭제 실패 - 잘못된 사용자")
     public void deletePostFailDiffMember() throws Exception {
         TestUtil.performRequest(mockMvc,
                 POST_API_URL + "/" + getLatestPostIdByMemberId("PostUser"), null, "DELETE", 200, 400);
