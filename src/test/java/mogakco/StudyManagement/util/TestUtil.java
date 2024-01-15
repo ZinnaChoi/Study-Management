adac7b3e985ca7acacf770b2b055e9d02403775f
Zinna <choiyh11097@gmail.com>
2024-01-15 12:07:12 +0900

    [fix] remove Component annotaion in TestUtil

--- src/test/java/mogakco/StudyManagement/util/TestUtil.java
+++ src/test/java/mogakco/StudyManagement/util/TestUtil.java
@@ -5,13 +5,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@Component
 public class TestUtil {
 
     public static void performPostRequest(MockMvc mockMvc, String url, String requestBodyJson, int expectedStatus,
