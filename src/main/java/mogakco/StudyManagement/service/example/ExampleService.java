7cc063eea4fe348b472ff941aac4ae35f6365d4c
Zinna <choiyh11097@gmail.com>
2024-01-19 16:15:03 +0900

    [chore] Remove Example Controller and service

--- src/main/java/mogakco/StudyManagement/service/example/ExampleService.java
+++
@@ -1,8 +0,0 @@
-package mogakco.StudyManagement.service.example;
-
-import mogakco.StudyManagement.service.common.LoggingService;
-
-public interface ExampleService {
-
-    void exampleMethod(String var1, LoggingService lo);
-}
