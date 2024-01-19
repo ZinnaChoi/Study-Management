7eaeecb9c9eb57c04c400e3aeb3c2fc910647910
Zinna <choiyh11097@gmail.com>
2024-01-19 16:11:33 +0900

    [chore] Remove unused controller and test

--- src/test/java/mogakco/StudyManagement/example/JunitTest.java
+++ src/test/java/mogakco/StudyManagement/example/JunitTest.java
@@ -1,5 +1,9 @@
 package mogakco.StudyManagement.example;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,6 +11,26 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 //ParameterizedTest template
 public class JunitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("BeforeAll: 테스트 실행 전 한 번만 실행");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("BeforeEach: 각 테스트 메소드 실행 전 실행");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("AfterEach: 각 테스트 메소드 실행 후 실행");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("AfterAll: 모든 테스트 실행 후 한 번만 실행");
+    }
 
     // @ValueSource를 사용한 문자열 테스트
     @ParameterizedTest
