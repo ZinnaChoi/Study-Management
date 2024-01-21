149ace10b3c6436f8a054fc5f7cd46f2b7cb5e91
MeMyself-And-I <leech_98@naver.com>
2024-01-21 15:11:04 +0900

    [refactor] remove unnecessary method from TestUtil

--- src/test/java/mogakco/StudyManagement/util/TestUtil.java
+++ src/test/java/mogakco/StudyManagement/util/TestUtil.java
@@ -52,49 +52,4 @@ public class TestUtil {
         return resultActions.andReturn();
     }
 
-    public static void performPostRequest(MockMvc mockMvc, String url, String requestBodyJson, int expectedStatus,
-            Integer expected)
-            throws Exception {
-        mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBodyJson))
-                .andExpect(status().is(expectedStatus))
-                .andExpect(expected != null ? jsonPath("$.retCode").value(expected) : null);
-    }
-
-    // Method OverLoading
-    public static void performPostRequest(MockMvc mockMvc, String url, String requestBodyJson, int expectedStatus,
-            Boolean expected, String expression)
-            throws Exception {
-        mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBodyJson))
-                .andExpect(status().is(expectedStatus))
-                .andExpect(expected != null ? jsonPath("$." + expression).value(expected) : null);
-    }
-
-    // Method OverLoading
-    public static void performPostRequest(MockMvc mockMvc, String url, String requestBodyJson, int expectedStatus,
-            String expected, String expression)
-            throws Exception {
-        mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBodyJson))
-                .andExpect(status().is(expectedStatus))
-                .andExpect(expected != null ? jsonPath("$." + expression).value(expected) : null);
-    }
-
-    public static MvcResult performGetRequest(MockMvc mockMvc, String url, int expectedStatus) throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(url)
-                .contentType(MediaType.APPLICATION_JSON));
-
-        if (expectedStatus == 200) {
-            resultActions.andExpect(status().isOk());
-        } else {
-            resultActions.andExpect(status().is(expectedStatus));
-        }
-
-        return resultActions.andReturn();
-    }
-
 }
