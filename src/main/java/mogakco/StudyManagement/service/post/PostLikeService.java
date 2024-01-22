47b63bb41ec361cd0c72a8197268f09b3a0cbdf6
Zinna <choiyh11097@gmail.com>
2024-01-22 22:02:24 +0900

    [feat] Add Post Like Registration API

---
+++ src/main/java/mogakco/StudyManagement/service/post/PostLikeService.java
@@ -0,0 +1,10 @@
+package mogakco.StudyManagement.service.post;
+
+import mogakco.StudyManagement.dto.DTOResCommon;
+import mogakco.StudyManagement.service.common.LoggingService;
+
+public interface PostLikeService {
+
+    DTOResCommon createPostLike(Long postId, LoggingService lo);
+
+}
