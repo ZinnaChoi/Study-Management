9819606c4f7fabee89ef8243ce1ca89172b8bb05
Zinna <choiyh11097@gmail.com>
2024-01-29 15:10:39 +0900

    [chore] Remove unused setter import in entity

--- src/main/java/mogakco/StudyManagement/domain/Member.java
+++ src/main/java/mogakco/StudyManagement/domain/Member.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import mogakco.StudyManagement.enums.MemberRole;
 
 @Entity
