f6633fd7499408564625f896be0760d04e4913d2
Zinna <choiyh11097@gmail.com>
2024-02-02 23:59:29 +0900

    [feat] Add Common Calendar to Absent Schedule page

--- src/main/frontend/src/pages/absent-schedule/AbsentSchedule.js
+++ src/main/frontend/src/pages/absent-schedule/AbsentSchedule.js
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { authClient } from "../../services/APIService";
 
 // 부재 일정 화면
 const AbsentSchedule = () => {
+  const [events] = useState([]);
+  const [schedules, setSchedules] = useState([]);
+
+  useEffect(() => {
+    const requestBody = {
+      sendDate: "20240112113804899",
+      systemId: "STUDY_0001",
+    };
+
+    authClient
+      .get("/schedules", { params: requestBody })
+      .then((response) => {
+        const sortedSchedules = response.data.registedSchedules.sort((a, b) =>
+          a.startTime.localeCompare(b.startTime)
+        );
+        setSchedules(sortedSchedules);
+      })
+      .catch((error) => {
+        console.error("스케쥴 조회 실패:", error);
+      });
+  }, []);
+
+  const containerStyle = {
+    display: "flex",
+  };
+
+  const scheduleStyle = {
+    flex: 1,
+    marginRight: "10px",
+  };
+
+  const calendarStyle = {
+    flex: 4,
+    height: "100vh",
+    maxWidth: "100%",
+    padding: 0,
+    margin: 0,
+    fontSize: "1.2em",
+  };
+
   return (
-    <React.Fragment>
-      <h1>부재 일정 화면</h1>
-    </React.Fragment>
+    <div>
+      <div style={containerStyle}>
+        <div style={scheduleStyle}>
+          <h3>스터디 시간</h3>
+          <ul>
+            {schedules.map((schedule) => (
+              <li key={schedule.scheduleId}>{schedule.scheduleName}</li>
+            ))}
+          </ul>
+        </div>
+        <div style={calendarStyle}>
+          <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            events={events}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
