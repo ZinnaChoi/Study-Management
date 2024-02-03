4fa9e257e4f9693954570838ecea6b0403752972
MeMyself-And-I <leech_98@naver.com>
2024-02-03 14:10:12 +0900

    [fix] StudyControllerTest issue fix

--- src/test/resources/study/StudySetup.sql
+++ src/test/resources/study/StudySetup.sql
@@ -3,7 +3,11 @@ INSERT INTO study.`member`
 VALUES
 ('010-1111-1111', DATE_FORMAT(NOW(6), '%Y%m%d%H%i%s%f'), DATE_FORMAT(NOW(6), '%Y%m%d%H%i%s%f'), 'user1', '사용자1', '$2a$10$khWE6krzNMGVRgpisGJmrOADrpklNtVEyGDVLJsHMb2UjyNNj7a0O', 'USER');
 
-insert into study_info(db_password, db_url, db_user, study_name, study_logo) values ('password', '10.10.110.110', 'admin', 'NotExistStudyName', null);
+
+INSERT INTO study_info(db_password, db_url, db_user, study_name, study_logo) 
+SELECT 'password', '10.10.110.110', 'admin', 'NotExistStudyName', null
+FROM dual
+WHERE NOT EXISTS (SELECT 1 FROM study_info);
 
 insert into schedule(end_time, schedule_name, start_time) values('13:00','NotExistScheduleName','14:00');
 
