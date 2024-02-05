f5371bd380fdcd8a0d0ba71549efad7ca75cf956
Zinna <choiyh11097@gmail.com>
2024-02-05 22:59:01 +0900

    [fix] FIx post list retrieve API to get all posts when the searchkeyword is blank

--- src/main/java/mogakco/StudyManagement/repository/PostRepository.java
+++ src/main/java/mogakco/StudyManagement/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package mogakco.StudyManagement.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,6 +10,8 @@ import mogakco.StudyManagement.domain.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+
+    Page<Post> findAll(Pageable pageable);
 
     boolean existsById(Long postId);
 }
