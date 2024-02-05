52b62bd5805d48a519feb5379e2270a65245f867
Zinna <choiyh11097@gmail.com>
2024-02-05 14:26:02 +0900

    [feat] Add member filtering for absence schedule retrieve

--- src/main/frontend/src/pages/absent-schedule/AbsentSchedule.js
+++ src/main/frontend/src/pages/absent-schedule/AbsentSchedule.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import qs from "qs";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -8,14 +9,24 @@ import { getCurrentDateTime } from "../../util/DateUtil";
 // 부재 일정 화면
 const AbsentSchedule = () => {
   const [events, setEvents] = useState([]);
+  const [membersList, setMembersList] = useState([]);
+  const [selectedMembers, setSelectedMembers] = useState([]);
   const [currentYearMonth, setCurrentYearMonth] = useState("");
   const [prevYearMonth, setPrevYearMonth] = useState("");
   const [colorMap, setColorMap] = useState({});
 
-  const getRandomPastelColor = () =>
-    `hsl(${360 * Math.random()}, ${60 + 40 * Math.random()}%, ${
-      70 + 30 * Math.random()
+  useEffect(() => {
+    // TODO: Call MemberList
+    const memberNames = ["지나", "엠마"];
+    setMembersList(memberNames);
+    setSelectedMembers(memberNames);
+  }, []);
+
+  const getRandomPastelColor = () => {
+    return `hsl(${360 * Math.random()}, ${25 + 70 * Math.random()}%, ${
+      85 + 10 * Math.random()
     }%)`;
+  };
 
   const getCurrentYearMonth = (dateInfo) => {
     const start = new Date(dateInfo.start);
@@ -30,29 +41,69 @@ const AbsentSchedule = () => {
     setCurrentYearMonth(`${year}${formattedMonth}`);
   };
 
+  function handleCheckboxChange(memberName) {
+    setSelectedMembers((prev) => {
+      const newSelectedMembers = prev.includes(memberName)
+        ? prev.filter((name) => name !== memberName)
+        : [...prev, memberName];
+      if (newSelectedMembers.length === 0) {
+        setEvents([]);
+      }
+      return newSelectedMembers;
+    });
+  }
+
+  const renderCheckboxes = () => {
+    return membersList.map((memberName) => (
+      <div key={memberName}>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedMembers.includes(memberName)}
+            onChange={() => handleCheckboxChange(memberName)}
+          />
+          {memberName}
+        </label>
+      </div>
+    ));
+  };
+
   useEffect(() => {
-    if (currentYearMonth !== prevYearMonth) {
+    if (
+      currentYearMonth &&
+      (currentYearMonth !== prevYearMonth || selectedMembers.length > 0)
+    ) {
       const requestBody = {
         sendDate: getCurrentDateTime(),
         systemId: "STUDY_0001",
         yearMonth: currentYearMonth,
+        memberNameList: selectedMembers,
       };
 
-      authClient
-        .get("/absent/calendar", { params: requestBody })
-        .then((absentResponse) => {
-          const newColorMap = { ...colorMap };
-          absentResponse.data.content.forEach((absentInfo) => {
-            const { scheduleName } = absentInfo;
-            if (!newColorMap[scheduleName]) {
-              newColorMap[scheduleName] = getRandomPastelColor();
-            }
-          });
+      const queryString = qs.stringify(requestBody, { arrayFormat: "repeat" });
 
+      authClient
+        .get("/absent/calendar?" + queryString)
+        .then((absentResponse) => {
+          const newColorMap = absentResponse.data.content.reduce(
+            (map, absentInfo) => {
+              const { scheduleName } = absentInfo;
+              map[scheduleName] = map[scheduleName] || getRandomPastelColor();
+              return map;
+            },
+            colorMap
+          );
           setColorMap(newColorMap);
 
-          const newEvents = absentResponse.data.content.map((absentInfo) => {
-            return {
+          const newEvents = absentResponse.data.content
+            .filter(
+              (absentInfo) =>
+                selectedMembers.length === 0 ||
+                absentInfo.memberNameList.some((name) =>
+                  selectedMembers.includes(name)
+                )
+            )
+            .map((absentInfo) => ({
               title: `${
                 absentInfo.scheduleName
               }: ${absentInfo.memberNameList.join(", ")}`,
@@ -60,8 +111,8 @@ const AbsentSchedule = () => {
               backgroundColor: newColorMap[absentInfo.scheduleName],
               borderColor: newColorMap[absentInfo.scheduleName],
               display: "block",
-            };
-          });
+            }));
+
           setEvents(newEvents);
           setPrevYearMonth(currentYearMonth);
         })
@@ -69,25 +120,44 @@ const AbsentSchedule = () => {
           console.error("데이터 조회 실패:", error);
         });
     }
-  }, [currentYearMonth, prevYearMonth, colorMap]);
+  }, [currentYearMonth, selectedMembers]);
 
-  const calendarStyle = {
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "row",
+  };
+
+  const checkboxContainerStyle = {
+    flex: 1,
+    borderRight: "1px solid #ccc",
+    paddingRight: "20px",
+    marginRight: "20px",
+  };
+
+  const calendarContainerStyle = {
+    flex: 5,
     height: "100vh",
     maxWidth: "100%",
     padding: 0,
     margin: 0,
-    fontSize: "1.2em",
+    fontSize: "1.0em",
   };
 
   return (
-    <div style={calendarStyle}>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        events={events}
-        datesSet={getCurrentYearMonth}
-        eventContent={renderEventContent}
-      />
+    <div style={containerStyle}>
+      <div style={checkboxContainerStyle}>
+        <h3>스터디원 선택</h3>
+        {renderCheckboxes()}
+      </div>
+      <div style={calendarContainerStyle}>
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          events={events}
+          datesSet={getCurrentYearMonth}
+          eventContent={renderEventContent}
+        />
+      </div>
     </div>
   );
 };
