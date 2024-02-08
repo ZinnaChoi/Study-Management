75e70071807e4e7a9635243dec5f894112d0792b
MeMyself-And-I <leech_98@naver.com>
2024-02-08 16:46:22 +0900

    [fix] study add & update swagger format issue resolve

--- src/main/java/mogakco/StudyManagement/controller/StudyController.java
+++ src/main/java/mogakco/StudyManagement/controller/StudyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,7 +18,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import mogakco.StudyManagement.dto.CommonRes;
 import mogakco.StudyManagement.dto.StudyInfoRes;
 import mogakco.StudyManagement.dto.StudyReq;
@@ -59,13 +59,38 @@ public class StudyController extends CommonController {
 
     @Operation(summary = "스터디 등록", description = "새 스터디 추가")
     @SecurityRequirement(name = "bearer-key")
-    @PostMapping(value = "/study", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/study", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public CommonRes createStudy(HttpServletRequest request,
-            @Valid @RequestPart(value = "studyReq") @Parameter(schema = @Schema(type = "string", format = "binary")) StudyReq studyReq,
-            @Parameter(description = "이미지 파일") @RequestPart(name = "logo file", required = false) MultipartFile imageFile) {
+            @Schema(name = "req", description = """
+                        스터디 추가 Request Body(json)
+                        studyName*(String): 추가할 스터디 이름
+                        schedules*(Array): 스케줄 리스트(HTML)
+                        schedules.scheduleName(String): 스케줄 이름
+                        schedules.startTime(String): 스케줄 시작 시간
+                        schedules.endTime(String): 스케줄 종료 시간
+                    """, required = true, example = """
+                        {
+                            "studyName": "모각코 스터디",
+                            "schedules": [
+                              {
+                                "scheduleName": "AM2",
+                                "startTime": "1300",
+                                "endTime": "1400"
+                              },
+                              {
+                                "scheduleName": "AM3",
+                                "startTime": "1500",
+                                "endTime": "1600"
+                              }
+                            ]
+                          }
+
+                    """) @RequestPart(name = "req") String req,
+            @Parameter(description = "이미지 파일") @RequestParam(name = "logo file", required = false) MultipartFile imageFile) {
         CommonRes result = new CommonRes();
 
         try {
+            StudyReq studyReq = mapper.readValue(req, StudyReq.class);
             startAPI(lo, studyReq);
             result = studyService.createStudy(studyReq, imageFile, lo);
         } catch (Exception e) {
@@ -80,13 +105,40 @@ public class StudyController extends CommonController {
 
     @Operation(summary = "스터디 정보 수정", description = "스터디 정보(스터디 이름, 로고, 스케줄) 수정")
     @SecurityRequirement(name = "bearer-key")
-    @PutMapping(value = "/study", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/study", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public CommonRes updateStudy(HttpServletRequest request,
-            @Valid @RequestPart(value = "studyReq") @Parameter(schema = @Schema(type = "string", format = "binary")) StudyReq studyReq,
-            @Parameter(description = "이미지 파일") @RequestPart(name = "logo file", required = false) MultipartFile imageFile) {
+            @Schema(name = "req", description = """
+                        스터디 추가 Request Body(json)
+                        studyName*(String): 현재 스터디 이름
+                        updateStudyName*(String): 변경할 스터디 이름
+                        schedules*(Array): 스케줄 리스트(HTML)
+                        schedules.scheduleName(String): 스케줄 이름
+                        schedules.startTime(String): 스케줄 시작 시간
+                        schedules.endTime(String): 스케줄 종료 시간
+                    """, required = true, example = """
+                        {
+                            "studyName": "현재 스터디 이름",
+                            "updateStudyName": "변경할 스터디 이름",
+                            "schedules": [
+                              {
+                                "scheduleName": "AM2",
+                                "startTime": "1300",
+                                "endTime": "1400"
+                              },
+                              {
+                                "scheduleName": "AM3",
+                                "startTime": "1500",
+                                "endTime": "1600"
+                              }
+                            ]
+                          }
+
+                    """) @RequestPart(name = "req") String req,
+            @Parameter(description = "이미지 파일") @RequestParam(name = "logo file", required = false) MultipartFile imageFile) {
         CommonRes result = new CommonRes();
 
         try {
+            StudyReq studyReq = mapper.readValue(req, StudyReq.class);
             startAPI(lo, studyReq);
             result = studyService.updateStudy(studyReq, imageFile, lo);
         } catch (Exception e) {
