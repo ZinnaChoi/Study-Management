0a1da24c0967bb816caf1a4d4633858349d84ffb
MeMyself-And-I <leech_98@naver.com>
2024-02-13 18:18:25 +0900

    [feat] login ui & logout api apply

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
@@ -11,11 +11,15 @@ import AttendanceStatistics from "./pages/study/AttendanceStatistics";
 import Management from "./pages/study/Management";
 import MembersInfo from "./pages/study/MembersInfo";
 import WakeUpSuccessRate from "./pages/study/WakeUpSuccessRate";
+import Login from "./pages/login/login";
+import Join from "./pages/join/join";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { menuTree } from "./constants/constants";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const token = localStorage.getItem("token");
 
 root.render(
   <React.Fragment>
@@ -23,47 +27,67 @@ root.render(
     <React.StrictMode>
       <div>
         <BrowserRouter>
-          <Header />
-          <Container
-            maxWidth="lg"
-            style={{
-              minHeight: "80vh",
-            }}
-          >
-            <Routes>
-              <Route
-                path={menuTree.main.path}
-                element={<MembersInfo />}
-              ></Route>
-              <Route
-                path={menuTree.absentSchedule.path}
-                element={<AbsentSchedule />}
-              ></Route>
-              <Route path={menuTree.myPage.path} element={<MyPage />}></Route>
-              <Route
-                path={menuTree.noticeBoard.path}
-                element={<NoticeBoard />}
-              ></Route>
-              <Route
-                path={menuTree.statistics.attendanceStatistics.path}
-                element={<AttendanceStatistics />}
-              ></Route>
-              <Route
-                path={menuTree.statistics.wakeupSuccessRate.path}
-                element={<WakeUpSuccessRate />}
-              ></Route>
-              <Route
-                path={menuTree.management.path}
-                element={<Management />}
-              ></Route>
-              {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-              <Route
-                path={menuTree.notFound.path}
-                element={<NotFound />}
-              ></Route>
-            </Routes>
-          </Container>
-          <Footer />
+          {/* 로그인 페이지를 위한 라우트 */}
+          <Routes>
+            <Route path={menuTree.join.path} element={<Join />} />
+            <Route path={menuTree.login.path} element={<Login />} />
+            {/* 기존의 메인 페이지 라우트들 */}
+            <Route
+              path="*"
+              element={
+                token ? (
+                  <React.Fragment>
+                    <Header />
+                    <Container
+                      maxWidth="lg"
+                      style={{
+                        minHeight: "80vh",
+                      }}
+                    >
+                      <Routes>
+                        <Route
+                          path={menuTree.main.path}
+                          element={<MembersInfo />}
+                        ></Route>
+                        <Route
+                          path={menuTree.absentSchedule.path}
+                          element={<AbsentSchedule />}
+                        ></Route>
+                        <Route
+                          path={menuTree.myPage.path}
+                          element={<MyPage />}
+                        ></Route>
+                        <Route
+                          path={menuTree.noticeBoard.path}
+                          element={<NoticeBoard />}
+                        ></Route>
+                        <Route
+                          path={menuTree.statistics.attendanceStatistics.path}
+                          element={<AttendanceStatistics />}
+                        ></Route>
+                        <Route
+                          path={menuTree.statistics.wakeupSuccessRate.path}
+                          element={<WakeUpSuccessRate />}
+                        ></Route>
+                        <Route
+                          path={menuTree.management.path}
+                          element={<Management />}
+                        ></Route>
+                        {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+                        <Route
+                          path={menuTree.notFound.path}
+                          element={<NotFound />}
+                        ></Route>
+                      </Routes>
+                    </Container>
+                    <Footer />
+                  </React.Fragment>
+                ) : (
+                  <Navigate to={menuTree.login.path} replace />
+                )
+              }
+            />
+          </Routes>
         </BrowserRouter>
       </div>
     </React.StrictMode>
