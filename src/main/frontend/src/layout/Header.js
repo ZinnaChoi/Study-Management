d368375033edc7f4db6a329dafbd5ece7f0fc4ee
MeMyself-And-I <leech_98@naver.com>
2024-02-26 16:29:04 +0900

    [feat] 1. if role is admin then study info menu enable 2. if study info is not registed then alert by role 3. if study info is not registed then redirect to study regist menu

--- src/main/frontend/src/layout/Header.js
+++ src/main/frontend/src/layout/Header.js
@@ -13,13 +13,14 @@ import {
   Tooltip,
   MenuItem,
 } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { menuTree } from "../constants/constants";
 import { authClient } from "../services/APIService";
 import HomeIcon from "@mui/icons-material/Home";
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
   const [anchorElStudy, setAnchorElStudy] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const [logo, setLogo] = React.useState(null);
@@ -42,8 +43,7 @@ function Header() {
 
   const doLogout = () => {
     navigate(menuTree.login.path);
-    localStorage.removeItem("token");
-    localStorage.removeItem("role");
+    clearAuthData();
     alert("로그아웃 되었습니다.");
   };
 
@@ -63,19 +63,47 @@ function Header() {
     );
   };
 
-  function setLogoImg() {
+  function setStudyInfo() {
     authClient
       .get("/study")
       .then(function (response) {
         setLogo(response.data?.logo);
+        if (
+          !response.data?.studyName &&
+          location.pathname !== menuTree.management.path
+        ) {
+          const role = localStorage.getItem("role");
+          if (role === "ADMIN") {
+            const isConfirm = window.confirm(
+              "등록된 스터디 정보가 없습니다. \n스터디 등록을 진행하시겠습니까?"
+            );
+
+            isConfirm ? navigate(menuTree.management.path) : moveLoginPage();
+          } else if (role === "USER") {
+            alert(
+              "등록된 스터디 정보가 없습니다. \n스터디 관리자에게 문의하여 스터디를 등록 해주세요."
+            );
+            moveLoginPage();
+          }
+        }
       })
       .catch(function (error) {
         console.log(error);
       });
   }
 
+  function clearAuthData() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+  }
+
+  function moveLoginPage() {
+    clearAuthData();
+    navigate(menuTree.login.path);
+  }
+
   useEffect(() => {
-    setLogoImg();
+    setStudyInfo();
   });
 
   return (
@@ -180,13 +208,14 @@ function Header() {
               style={{
                 textDecoration: "none",
                 color: "inherit",
-                pointerEvents: menuTree.management.disabled ? "none" : "",
+                pointerEvents:
+                  localStorage.getItem("role") === "USER" ? "none" : "",
               }}
             >
               <Button
                 key={menuTree.management.path}
                 sx={{ color: "black", display: "block" }}
-                disabled={menuTree.management.disabled}
+                disabled={localStorage.getItem("role") === "USER"}
               >
                 {menuTree.management.name}
               </Button>
--- src/main/frontend/src/pages/study/Management.js
+++ src/main/frontend/src/pages/study/Management.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { authClient } from "../../services/APIService";
 
-// 부재 일정 화면
-const Management = () => {
+// 스터디 관리 화면
+export default function Management() {
+  const [isStudyInfoRegisted, setIsStudyInfoRegisted] = useState(false);
+  useEffect(() => {
+    getStudyInfo();
+  });
+
+  function getStudyInfo() {
+    authClient
+      .get("/study")
+      .then(function (response) {
+        if (response.data?.studyName) {
+          setIsStudyInfoRegisted(true);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <React.Fragment>
-      <h1>관리자 - 스터디 관리 화면</h1>
+      {isStudyInfoRegisted ? (
+        <h1>관리자 - 스터디 관리 화면</h1>
+      ) : (
+        <h1>관리자 - 스터디 등록 화면</h1>
+      )}
     </React.Fragment>
   );
-};
-
-export default Management;
+}
