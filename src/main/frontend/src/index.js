bea88eefa30aad3a2f8d4e749668f8018a4fcf65
MeMyself-And-I <leech_98@naver.com>
2024-02-28 16:00:46 +0900

    [chore] useless code delete

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -79,7 +79,6 @@ root.render(
                             <Management />
                           )
                         }
-                        // element={<Management />}
                       ></Route>
                       {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
                       <Route
