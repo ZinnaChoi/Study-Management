b8ca0e5e89d855db9e76a34ad3dc712beb7651c9
MeMyself-And-I <leech_98@naver.com>
2024-03-03 15:42:13 +0900

    [feat] if schedule add popup time validation fail then do not close that popup

--- src/main/frontend/src/pages/study/StudyRegistPopup.js
+++ src/main/frontend/src/pages/study/StudyRegistPopup.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Tooltip, Chip } from "@mui/material";
+import { Tooltip, Chip, Button } from "@mui/material";
 import CommonDialog from "../../components/CommonDialog";
 import "../../styles/Dialog.css";
 import "../../styles/StudyManagement.css";
 
 export default function StudyRegistPopup(props) {
   const [addedSchedules, setAddedSchedules] = useState([]);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   useEffect(() => {
     if (props.schedules) {
@@ -33,6 +34,7 @@ export default function StudyRegistPopup(props) {
       endTime: data["종료 시간"].replace(":", ""),
     };
     setAddedSchedules((prevSchedules) => [...prevSchedules, schedule]);
+    setDialogOpen(false);
   };
 
   function isValidTime(startTime, endTime) {
@@ -54,12 +56,21 @@ export default function StudyRegistPopup(props) {
     return true;
   }
 
+  const handleOpen = () => {
+    setDialogOpen(true);
+  };
+
   const SchedulesAdd = () => {
     return (
       <div>
         <div className="dialog-content fullwidth-content">
           <div className="study-select-title">스케줄</div>
+          <Button variant="outlined" onClick={handleOpen}>
+            추가
+          </Button>
           <CommonDialog
+            open={dialogOpen}
+            closeDialog={false}
             btnTitle={"추가"}
             title={"스케줄 등록"}
             names={["스케줄 이름", "시작 시간", "종료 시간"]}
@@ -67,8 +78,9 @@ export default function StudyRegistPopup(props) {
             inputTypes={["", "time", "time"]}
             acceptStr={"추가"}
             cancleStr={"취소"}
+            onClose={() => setDialogOpen(false)}
             submitEvt={addSchedule}
-            showButton={true}
+            showButton={false}
           ></CommonDialog>
           {addedSchedules.map((as, index) => (
             <Tooltip
