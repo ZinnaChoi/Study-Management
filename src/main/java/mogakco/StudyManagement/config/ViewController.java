f2909915f217959e98fdbcdc188f7ee107992d48
곤듀님 <dayeondayeon@naver.com>
2024-03-08 16:54:07 +0900

    [feat] seperate config files

--- src/main/java/mogakco/StudyManagement/config/ViewController.java
+++ src/main/java/mogakco/StudyManagement/config/ViewController.java
@@ -1,7 +1,6 @@
 package mogakco.StudyManagement.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,13 +13,6 @@ public class ViewController implements WebMvcConfigurer {
     registry
         .addViewController("/{path:[^\\.]*}")
         .setViewName("forward:/index.html");
-  }
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("http://localhost:8090", "http://localhost:3000")
-        .allowedMethods("GET", "POST", "PUT", "DELETE");
   }
 
 }
