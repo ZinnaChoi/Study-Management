8d46fbb610d569cfded9ae036f30589040807e73
Zinna <choiyh11097@gmail.com>
2024-03-20 10:28:20 +0900

    [refactor] Remove duplicate declaration in jasypt config test

--- src/test/java/mogakco/StudyManagement/config/JasyptConfigTest.java
+++ src/test/java/mogakco/StudyManagement/config/JasyptConfigTest.java
@@ -21,6 +21,8 @@ public class JasyptConfigTest {
         String secretKey = "secretKey";
         String adminId = "adminId";
         String adminPassword = "adminPassword";
+        String mailUsername = "mailUsername";
+        String mailPassword = "mailPassword";
 
         System.out.println("En_dbUrl : " + jasyptEncoding(dbUrl));
         System.out.println("En_dbUsername : " + jasyptEncoding(dbUsername));
@@ -28,6 +30,8 @@ public class JasyptConfigTest {
         System.out.println("En_secretKey : " + jasyptEncoding(secretKey));
         System.out.println("En_adminId : " + jasyptEncoding(adminId));
         System.out.println("En_adminPassword : " + jasyptEncoding(adminPassword));
+        System.out.println("En_mailUsername : " + jasyptEncoding(mailUsername));
+        System.out.println("En_mailPassword : " + jasyptEncoding(mailPassword));
     }
 
     @Test
@@ -38,6 +42,8 @@ public class JasyptConfigTest {
         String secretKey = "fQilxnWt3Un46+b/Ir92UlSCrc4G8JSu"; // secretKey
         String adminId = "nyhunuGSFeKvK5hKK8D0dQ=="; // adminId
         String adminPassword = "UJXZ1c5ujFKk1BqMBoL1K65zjrez1zKh"; // adminPassword
+        String mailUsername = "QUHo96L7Ot0dlkh9XMHIZ/+OkPW3YTTd"; // mailUsername
+        String mailPassword = "oguHT+kCzg39p6FjITfrRTyStrh2J8lK"; // mailPassword
 
         System.out.println("DE_dbUrl : " + jasyptDecoding(dbUrl));
         System.out.println("DE_dbUsername : " + jasyptDecoding(dbUsername));
@@ -45,20 +51,23 @@ public class JasyptConfigTest {
         System.out.println("DE_secretKey : " + jasyptDecoding(secretKey));
         System.out.println("DE_adminId : " + jasyptDecoding(adminId));
         System.out.println("DE_adminPassword : " + jasyptDecoding(adminPassword));
+        System.out.println("DE_mailUsername : " + jasyptDecoding(mailUsername));
+        System.out.println("DE_mailPassword : " + jasyptDecoding(mailPassword));
+
     }
 
-    public String jasyptEncoding(String value) {
+    private String jasyptEncoding(String value) {
+        return jasyptProcessor(true, value);
+    }
+
+    private String jasyptDecoding(String value) {
+        return jasyptProcessor(false, value);
+    }
+
+    private String jasyptProcessor(boolean encrypt, String value) {
         StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
         pbeEnc.setAlgorithm("PBEWithMD5AndDES");
         pbeEnc.setPassword(password);
-        return pbeEnc.encrypt(value);
+        return encrypt ? pbeEnc.encrypt(value) : pbeEnc.decrypt(value);
     }
-
-    public String jasyptDecoding(String value) {
-        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword(password);
-        return pbeEnc.decrypt(value);
-    }
-
 }
