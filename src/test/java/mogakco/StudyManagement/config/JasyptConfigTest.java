4f745438fed036e625319cb22defcfb86c68928a
Zinna <choiyh11097@gmail.com>
2024-03-20 09:54:34 +0900

    [fix] Encrypt db connection info

--- src/test/java/mogakco/StudyManagement/config/JasyptConfigTest.java
+++ src/test/java/mogakco/StudyManagement/config/JasyptConfigTest.java
@@ -3,45 +3,61 @@ package mogakco.StudyManagement.config;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DisplayName("암복호화 테스트")
+@DisplayName("암복호화 테스트.")
+@SpringBootTest
 public class JasyptConfigTest {
 
-    public String key = "code123lovers!#@";
+    @Value("${jasypt.encryptor.password}")
+    private String password;
 
     @Test
     void stringEncryptor() {
-        String secretKey = "NwkqmAFTauzXEOiJLxcMRHSbYDthgWCsPre";
-        String id = "admin";
-        String password = "123password!@#";
+        String dbUrl = "dbUrl";
+        String dbUsername = "dbUsername";
+        String dbPassword = "dbPassword!";
+        String secretKey = "secretKey";
+        String adminId = "adminId";
+        String adminPassword = "adminPassword";
 
+        System.out.println("En_dbUrl : " + jasyptEncoding(dbUrl));
+        System.out.println("En_dbUsername : " + jasyptEncoding(dbUsername));
+        System.out.println("En_dbPassword : " + jasyptEncoding(dbPassword));
         System.out.println("En_secretKey : " + jasyptEncoding(secretKey));
-        System.out.println("En_id: " + jasyptEncoding(id));
-        System.out.println("En_password : " + jasyptEncoding(password));
+        System.out.println("En_adminId : " + jasyptEncoding(adminId));
+        System.out.println("En_adminPassword : " + jasyptEncoding(adminPassword));
     }
 
     @Test
     void stringDecryptor() {
-        String secretKey = "ImYd1fl8W/X7WGcMw32y33gu2PQ+94lLkBlmD36ks6I5zeZnq93mGTR8fsEDN0oj";
-        String id = "RYifAkG+MdVjdbTZY1AQDw==";
-        String password = "QFyCIGUGCTk+kMxIE0Mu7kDky/HlwhEC";
+        String dbUrl = "dbUrl";
+        String dbUsername = "dbUsername";
+        String dbPassword = "dbPassword!";
+        String secretKey = "secretKey";
+        String adminId = "adminId";
+        String adminPassword = "adminPassword";
 
+        System.out.println("DE_dbUrl : " + jasyptDecoding(dbUrl));
+        System.out.println("DE_dbUsername : " + jasyptDecoding(dbUsername));
+        System.out.println("DE_dbPassword : " + jasyptDecoding(dbPassword));
         System.out.println("DE_secretKey : " + jasyptDecoding(secretKey));
-        System.out.println("DE_id : " + jasyptDecoding(id));
-        System.out.println("DE_password : " + jasyptDecoding(password));
+        System.out.println("DE_adminId : " + jasyptDecoding(adminId));
+        System.out.println("DE_adminPassword : " + jasyptDecoding(adminPassword));
     }
 
     public String jasyptEncoding(String value) {
         StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
         pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword(key);
+        pbeEnc.setPassword(password);
         return pbeEnc.encrypt(value);
     }
 
     public String jasyptDecoding(String value) {
         StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
         pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword(key);
+        pbeEnc.setPassword(password);
         return pbeEnc.decrypt(value);
     }
 
